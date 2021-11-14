bcfb1afeb77e0a9f52c2bafa06a2fcce35545eff
strawbberrys <nicholascorrado0046@gmail.com>
2021-11-14 18:36:34 -0500

    add httpenabled to project

--- src/modules/converter.lua
+++ src/modules/converter.lua
@@ -4,11 +4,16 @@ local property_parser = require(script.Parent.DumpParser)
 local converter = {}
 
 do
+    function converter.BuildProperties(instance)
+        
+    end
+
     function converter.Convert(instance, convert_single)
         local converted = ""
 
+        local instance_dictonary = {}
         for index, object in ipairs(instance:GetDescendants()) do
-            
+
         end
     end
 end
